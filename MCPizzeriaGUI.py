891a9245a1ff4ab72ee5dea1cb5aa8f568d20ca4
simon09ja <simon09janssen@gmail.com>
2026-04-07 09:28:13 +0200

    begin gui

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -32,3 +32,44 @@ venster.wm_title("MC Pizzeria")
 
 #reageert op gebruikersinvoer, deze regel als laatste laten staan
 venster.mainloop()
+# Dit bestand zorgt voor de gebruikersinterface (GUI)van onze programma.
+# Vul hier de naam van je programma in:
+#
+#
+# Vul hier jullie namen in:
+# Lucas
+#
+#
+
+
+### --------- Bibliotheken en globale variabelen -----------------
+from tkinter import *
+import MCPizzeriaSQL
+
+
+### ---------  Functie definities  -----------------
+
+
+### --------- Hoofdprogramma  ---------------
+
+venster = Tk()
+venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
+venster.wm_title("MC Pizzeria")
+
+
+
+labelIntro = Label(venster, text="Welkom!")
+labelIntro.grid(row=0, column=0, sticky="W")
+knopSluit = Button(venster, text="Sluiten",width=12,command=venster.destroy)
+knopSluit.grid(row=17, column=4)
+
+
+
+
+
+
+
+
+
+#reageert op gebruikersinvoer, deze regel als laatste laten staan
+venster.mainloop()
--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -15,41 +15,60 @@ with sqlite3.connect("MCPizzeria.db") as db:
 
 
 ### ---------  Functie definities  -----------------
-def maakTabellenAan(): # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs 
-        cursor.execute(""" 
-                    CREATE TABLE IF NOT EXISTS tbl_pizzas( 
-                        gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
-                        gerechtNaam TEXT NOT NULL, 
-                        gerechtPrijs REAL NOT NULL);""") 
-        print("Tabel 'tbl_pizzas' aangemaakt.") 
-
-maakTabellenAan()
-
 def printTabel(tabel_naam):
-      cursor.execute("SELECT * FROM " + tabel_naam) #SQL om ALLE gegevens te halen 
-      opgehaalde_gegevens = cursor.fetchall() #sla gegevens op in een variabele 
-      print("Tabel " + tabel_naam + ":", opgehaalde_gegevens) #druk gegevens af
+    cursor.execute("SELECT * FROM " + tabel_naam) #SQL om ALLE gegevens te halen
+    opgehaalde_gegevens = cursor.fetchall() #sla gegevens op in een variabele
+    print("Tabel " + tabel_naam + ":", opgehaalde_gegevens) #druk gegevens af
 
-printTabel("tbl_pizzas") 
+def maakTabellenAan():
+ # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs
+ cursor.execute("""
+ CREATE TABLE IF NOT EXISTS tbl_pizzas(
+ gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+ gerechtNaam TEXT NOT NULL,
+ gerechtPrijs REAL NOT NULL);""")
+ print("Tabel 'tbl_pizzas' aangemaakt.")
+ cursor.execute("""
+ CREATE TABLE IF NOT EXISTS tbl_klanten(
+ klantNr INTEGER PRIMARY KEY AUTOINCREMENT,
+ klantAchternaam TEXT);""")
+ print("Tabel 'tbl_klanten' aangemaakt.")
 
-def voegPizzaToe(naam_nieuwe_pizza, prijs_nieuwe_pizza): 
-    cursor.execute("INSERT INTO tbl_pizzas VALUES(NULL, ?, ? )", (naam_nieuwe_pizza, prijs_nieuwe_pizza)) 
-    db.commit() #gegevens naar de database wegschrijven 
-
-    print("Pizza toegevoegd:") 
-    printTabel("tbl_pizzas") 
-
-voegPizzaToe("Margarita", 9.50)
-voegPizzaToe("Hawaii", 12.25)
-voegPizzaToe("Salami", 10.00)
-
+def voegPizzaToe(naam_nieuwe_pizza, prijs_nieuwe_pizza):
+    cursor.execute("INSERT INTO tbl_pizzas VALUES(NULL, ?, ? )", (naam_nieuwe_pizza, prijs_nieuwe_pizza))
+    db.commit() #gegevens naar de database wegschrijven
+    print("Pizzas toegevoegd:")
+    printTabel("tbl_pizzas")
 
 def verwijderPizza(gerechtNaam):
-      cursor.execute("DELETE FROM tbl_pizzas WHERE gerechtNaam = ?", (gerechtNaam,)) 
-      print("Gerecht verwijderd uit 'tbl_pizzas':", gerechtNaam ) 
-      db.commit() #gegevens naar de database wegschrijven 
-      printTabel("tbl_pizzas") 
+   cursor.execute("DELETE FROM tbl_pizzas WHERE gerechtNaam = ?", (gerechtNaam,))
+   print("Gerecht verwijderd uit 'tbl_pizzas':", gerechtNaam )
+   db.commit() #gegevens naar de database wegschrijven
+   printTabel("tbl_pizzas")
 
-verwijderPizza("Hawaii")
+def pasGerechtAan(gerechtID, nieuweGerechtNaam, nieuwePrijs):
+ cursor.execute("UPDATE tbl_pizzas SET gerechtNaam = ?, gerechtPrijs = ? WHERE gerechtID = ?", (nieuweGerechtNaam, nieuwePrijs, gerechtID ))
+ db.commit() #gegevens naar de database wegschrijven
+ print("Gerecht aangepast")
+ printTabel("tbl_pizzas")
+
+
+def voegKlantToe(naam_nieuwe_klant):
+ cursor.execute("INSERT INTO tbl_klanten VALUES(NULL, ?)", (naam_nieuwe_klant,))
+ db.commit()
+ print("Klant toegevoegd:")
+ printTabel('tbl_klanten')
+
+
+
+
 ### --------- Hoofdprogramma  ---------------
-
+maakTabellenAan()
+printTabel("tbl_pizzas")
+voegPizzaToe("Margarita", 9.5)
+voegPizzaToe("Hawaii", 12.25)
+voegPizzaToe("Salami", 10.0)
+verwijderPizza("Hawaii")
+pasGerechtAan(3, "Salamiiii", 19.25)
+voegKlantToe("Yahu")
+voegKlantToe("Trump")
