7342a9e1cb9ffe3f1b2e013aa342567f53bf4f7f
simon09ja <simon09janssen@gmail.com>
2026-04-14 09:15:51 +0200

    ZOekklantinintabel bezig

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -1,4 +1,4 @@
-# Dit bestand zorgt voor de gebruikersinterface (GUI)van onze programma.
+# Dit bestand zorgt voor de gebruikersinterface (GUI) van onze programma.
 # Vul hier de naam van je programma in:
 #
 #
@@ -14,62 +14,37 @@ import MCPizzeriaSQL
 
 
 ### ---------  Functie definities  -----------------
+def zoekKlant():
+    gevonden_klanten = MCPizzeriaSQL.zoekKlantInTabel(ingevoerde_klantnaam.get())
+    if gevonden_klanten == []:
+        labelResultaat.config(text="Geen klant gevonden.")
+    else:
+        labelResultaat.config(text="Gevonden: " + str(gevonden_klanten))
 
 
 ### --------- Hoofdprogramma  ---------------
 
 venster = Tk()
-venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
+venster.iconbitmap("MC_icon.ico")
 venster.wm_title("MC Pizzeria")
-
-
-
-
-
-
-
-
-
-#reageert op gebruikersinvoer, deze regel als laatste laten staan
-venster.mainloop()
-# Dit bestand zorgt voor de gebruikersinterface (GUI)van onze programma.
-# Vul hier de naam van je programma in:
-#
-#
-# Vul hier jullie namen in:
-# Lucas
-#
-#
-
-
-### --------- Bibliotheken en globale variabelen -----------------
-from tkinter import *
-import MCPizzeriaSQL
-
-
-### ---------  Functie definities  -----------------
-
-
-### --------- Hoofdprogramma  ---------------
-
-venster = Tk()
-venster.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
-venster.wm_title("MC Pizzeria")
-
-
 
 labelIntro = Label(venster, text="Welkom!")
 labelIntro.grid(row=0, column=0, sticky="W")
-knopSluit = Button(venster, text="Sluiten",width=12,command=venster.destroy)
-knopSluit.grid(row=17, column=4)
+knopSluit = Button(venster, text="Sluiten", width=12, command=venster.destroy)
+knopSluit.grid(row=17, column=2)
 
+labelNaam = Label(venster, text="Klantnaam:")
+labelNaam.grid(row=1, column=0)
 
+ingevoerde_klantnaam = StringVar()
 
+invoerveldKlantnaam = Entry(venster, textvariable=ingevoerde_klantnaam)
+invoerveldKlantnaam.grid(row=1, column=1, sticky="W")
 
+knopZoekOpKlantnaam = Button(venster, text="Zoek klant", width=12, command=zoekKlant)
+knopZoekOpKlantnaam.grid(row=1, column=14)
 
+labelResultaat = Label(venster, text="")
+labelResultaat.grid(row=2, column=0, columnspan=5, sticky="W")
 
-
-
-
-#reageert op gebruikersinvoer, deze regel als laatste laten staan
 venster.mainloop()
--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -11,55 +11,60 @@
 
 import sqlite3
 with sqlite3.connect("MCPizzeria.db") as db:
-    cursor = db.cursor()#cursor is object waarmee je data uit de database kan halen
+    cursor = db.cursor()
 
 
 ### ---------  Functie definities  -----------------
 def printTabel(tabel_naam):
-    cursor.execute("SELECT * FROM " + tabel_naam) #SQL om ALLE gegevens te halen
-    opgehaalde_gegevens = cursor.fetchall() #sla gegevens op in een variabele
-    print("Tabel " + tabel_naam + ":", opgehaalde_gegevens) #druk gegevens af
+    cursor.execute("SELECT * FROM " + tabel_naam)
+    opgehaalde_gegevens = cursor.fetchall()
+    print("Tabel " + tabel_naam + ":", opgehaalde_gegevens)
 
 def maakTabellenAan():
- # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs
- cursor.execute("""
- CREATE TABLE IF NOT EXISTS tbl_pizzas(
- gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
- gerechtNaam TEXT NOT NULL,
- gerechtPrijs REAL NOT NULL);""")
- print("Tabel 'tbl_pizzas' aangemaakt.")
- cursor.execute("""
- CREATE TABLE IF NOT EXISTS tbl_klanten(
- klantNr INTEGER PRIMARY KEY AUTOINCREMENT,
- klantAchternaam TEXT);""")
- print("Tabel 'tbl_klanten' aangemaakt.")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tbl_pizzas(
+    gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+    gerechtNaam TEXT NOT NULL,
+    gerechtPrijs REAL NOT NULL);""")
+    print("Tabel 'tbl_pizzas' aangemaakt.")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tbl_klanten(
+    klantNr INTEGER PRIMARY KEY AUTOINCREMENT,
+    klantAchternaam TEXT);""")
+    print("Tabel 'tbl_klanten' aangemaakt.")
 
 def voegPizzaToe(naam_nieuwe_pizza, prijs_nieuwe_pizza):
-    cursor.execute("INSERT INTO tbl_pizzas VALUES(NULL, ?, ? )", (naam_nieuwe_pizza, prijs_nieuwe_pizza))
-    db.commit() #gegevens naar de database wegschrijven
+    cursor.execute("INSERT INTO tbl_pizzas VALUES(NULL, ?, ?)", (naam_nieuwe_pizza, prijs_nieuwe_pizza))
+    db.commit()
     print("Pizzas toegevoegd:")
     printTabel("tbl_pizzas")
 
 def verwijderPizza(gerechtNaam):
-   cursor.execute("DELETE FROM tbl_pizzas WHERE gerechtNaam = ?", (gerechtNaam,))
-   print("Gerecht verwijderd uit 'tbl_pizzas':", gerechtNaam )
-   db.commit() #gegevens naar de database wegschrijven
-   printTabel("tbl_pizzas")
+    cursor.execute("DELETE FROM tbl_pizzas WHERE gerechtNaam = ?", (gerechtNaam,))
+    print("Gerecht verwijderd uit 'tbl_pizzas':", gerechtNaam)
+    db.commit()
+    printTabel("tbl_pizzas")
 
 def pasGerechtAan(gerechtID, nieuweGerechtNaam, nieuwePrijs):
- cursor.execute("UPDATE tbl_pizzas SET gerechtNaam = ?, gerechtPrijs = ? WHERE gerechtID = ?", (nieuweGerechtNaam, nieuwePrijs, gerechtID ))
- db.commit() #gegevens naar de database wegschrijven
- print("Gerecht aangepast")
- printTabel("tbl_pizzas")
-
+    cursor.execute("UPDATE tbl_pizzas SET gerechtNaam = ?, gerechtPrijs = ? WHERE gerechtID = ?", (nieuweGerechtNaam, nieuwePrijs, gerechtID))
+    db.commit()
+    print("Gerecht aangepast")
+    printTabel("tbl_pizzas")
 
 def voegKlantToe(naam_nieuwe_klant):
- cursor.execute("INSERT INTO tbl_klanten VALUES(NULL, ?)", (naam_nieuwe_klant,))
- db.commit()
- print("Klant toegevoegd:")
- printTabel('tbl_klanten')
+    cursor.execute("INSERT INTO tbl_klanten VALUES(NULL, ?)", (naam_nieuwe_klant,))
+    db.commit()
+    print("Klant toegevoegd:")
+    printTabel('tbl_klanten')
 
-
+def zoekKlantInTabel(ingevoerde_klantnaam):
+    cursor.execute("SELECT * FROM tbl_klanten WHERE klantAchternaam = ?", (ingevoerde_klantnaam,))
+    zoek_resultaat = cursor.fetchall()
+    if zoek_resultaat == []:
+        print("Geen klant gevonden met achternaam", ingevoerde_klantnaam)
+        return []  # Stop hier, voeg NIET toe
+    print("Klant gevonden:", zoek_resultaat)
+    return zoek_resultaat
 
 
 ### --------- Hoofdprogramma  ---------------
@@ -70,5 +75,8 @@ voegPizzaToe("Hawaii", 12.25)
 voegPizzaToe("Salami", 10.0)
 verwijderPizza("Hawaii")
 pasGerechtAan(3, "Salamiiii", 19.25)
-voegKlantToe("Yahu")
-voegKlantToe("Trump")
+
+voegKlantToe("Janssen")
+voegKlantToe("Bakker")
+voegKlantToe("Lamari")
+voegKlantToe("OOM6")
